e3bc273c21453847997a552cae6e72adf95e103a
MoonChain <moonchainproject@gmail.com>
2025-11-07 21:45:38 +0700

    fix build path for render

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../backend/src/dist', // build vào đúng chỗ backend đang serve
+    emptyOutDir: true,
+  },
 })
